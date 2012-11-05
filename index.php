395a2df796ff3eb69e26905101ab0ab8a3b9798c
Warz <warzmx@gmail.com>
2012-11-05 01:05:40 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Bread is tasty</title>
+    </head>
+    <body>
+        <p>What kind of bread is most tasty? Which flavor is optimal and what should be the price?</p>
+    </body>
+</html>
